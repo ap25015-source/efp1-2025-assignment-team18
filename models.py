25b665399555c9ded9029941627646f0534ff25f
theoni kasviki <ap25015@hua.gr>
2026-02-18 01:00:48 +0200

    models.py

---
+++ models.py
@@ -0,0 +1,52 @@
+class Customer: # Represents a customer
+    def __init__(self, customer_id: int, fitting_room_id: int, loyalty_card_id: int, request_support: str):
+        self.customer_id = customer_id
+        self.fitting_room_id = fitting_room_id
+        self.loyalty_card_id=loyalty_card_id 
+        self.request_support= request_support
+
+    def __str__(self) -> str: # String representation of the customer
+        return f"{self.customer_id}: {self.fitting_room_id} {self.loyalty_card_id} {self.request_support}"
+
+
+class Employee: # Represents an Employee
+    def __init__(self, employee_id: int, availability: str):
+        self.employee_id = employee_id
+        self.availability = availability
+       
+
+    def is_available(self) -> bool:
+        # TODO: επιστρέψτε True αν υπάρχει διαθεσιμότητα  υπαλλήλου
+        return self.availability
+
+    #ίσως το χρειαστούμε
+    def add_participant(self) -> None:
+        # TODO: αυξήστε τον αριθμό συμμετεχόντων κατά 1
+        self.current_participants += 1
+
+    def __str__(self) -> str: # String representation of the class session
+        return (f"{self.session_id}: {self.title} "
+                f"({self.date} {self.time}) "
+                f"[{self.current_participants}/{self.capacity}]")
+
+class NotificationService:
+    def __init__(self, service_id: int):
+        self.service_id = service_id
+
+#μπορεί να το χρειαστούμε
+    def __str__(self) -> str:
+        return (f"Booking {self.booking_id} - {self.member.name} "
+                f"-> {self.session.title} ({self.session.date} {self.session.time})")
+    
+    class ClothingItem: # Represents a garment
+        def __init__(self, barcode: int, description: str, brand: str, photos: list, available_colours: str, price: float):
+            self.barcode = barcode
+            self.description = description
+            self.brand = brand 
+            self.photos = photos
+            self.available_colours = available_colours
+            self.price = price
+
+    class ClothingManagementSystem:
+        def __init__(self, cms_id: int):
+            self. cms_id = cms_id
