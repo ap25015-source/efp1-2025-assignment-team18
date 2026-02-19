18a332f75a0594bb3267c74ee689adf1d6d9177c
Μαρία <ap25022@hua.gr>
2026-02-19 23:27:08 +0200

    Αλλάξαμε τα models και services, και κάναμε τον κώδικα για τους employee στο services.py

--- models.py
+++ models.py
@@ -1,40 +1,30 @@
 class Customer: # Represents a customer
-    def __init__(self, customer_id: int, fitting_room_id: int, loyalty_card_id: int, request_support: str):
+    def __init__(self, customer_id: int, name: str, phone_number: int, loyalty_card_id: int):
         self.customer_id = customer_id
-        self.fitting_room_id = fitting_room_id
+        self.name = name
+        self.phone_number = phone_number
         self.loyalty_card_id=loyalty_card_id 
-        self.request_support= request_support
 
     def __str__(self) -> str: # String representation of the customer
         return f"{self.customer_id}: {self.fitting_room_id} {self.loyalty_card_id} {self.request_support}"
 
 
 class Employee: # Represents an Employee
-    def __init__(self, employee_id: int, availability: str):
+    def __init__(self, employee_id: int, availability: bool, name: str, phone_number: int):
         self.employee_id = employee_id
         self.availability = availability
-       
+        self.name = name
+        self.phone_number = phone_number
 
     def is_available(self) -> bool:
         # TODO: επιστρέψτε True αν υπάρχει διαθεσιμότητα  υπαλλήλου
         return self.availability
-
-    #ίσως το χρειαστούμε
-    def add_participant(self) -> None:
-        # TODO: αυξήστε τον αριθμό συμμετεχόντων κατά 1
-        self.current_participants += 1
-
-    def __str__(self) -> str: # String representation of the class session
-        return (f"{self.session_id}: {self.title} "
-                f"({self.date} {self.time}) "
-                f"[{self.current_participants}/{self.capacity}]")
-
-
-#μπορεί να το χρειαστούμε
-    def __str__(self) -> str:
-        return (f"Booking {self.booking_id} - {self.member.name} "
-                f"-> {self.session.title} ({self.session.date} {self.session.time})")
     
+    def emp_id(self) -> int:
+        # Επιστρέφει το ID του υπαλλήλου
+        return self.employee_id
+
+
 class ClothingItem: # Represents a garment
     def __init__(self, clothingitem_id:int, barcode: int, description: str, brand: str, photos: list, available_colours: str, price: float):
         self.clothingitem_id= clothingitem_id
--- services.py
+++ services.py
@@ -3,7 +3,7 @@ from models import Customer, Employee, ClothingItem
 
 class SupportSystem
     def __init__(self):
-        self.customers: list[customer] = [] # List to store customers
+        self.customers: list[Customer] = [] # List to store customers
         self.employees: list[Employee] = [] # List to store employees
         self.clothingitems: list[ClothingItem] = [] # List to store clothingitems
 
@@ -17,20 +17,20 @@ class SupportSystem
         """Δημιουργεί 2-3 έτοιμους υπαλλήλους για τις δοκιμές."""
         self.add_employee("Maria", "6973703687")
         self.add_employee("Spyros", "6985367592")
-        self.add_clothingitem("benetton", "5296347", "κόκκινο", "t-shirt","25", url="https://www.kathararoucha.gr/gynaikeio-aplo-mplouzaki/to-kokkino/2XL/?gad_source")
-        self.add_clothingitem("adidas", "2579321", "μπλε", "φορμα", "50", url="https://www.aboutyou.gr/p/adidas-sportswear/panteloni-formas-essentials-26229905?vid")
+        self.add_clothingitem("benetton", "5296347", "red", "t-shirt","25", url="https://www.kathararoucha.gr/gynaikeio-aplo-mplouzaki/to-kokkino/2XL/?gad_source")
+        self.add_clothingitem("adidas", "2579321", "blue", "tracksuit", "50", url="https://www.aboutyou.gr/p/adidas-sportswear/panteloni-formas-essentials-26229905?vid")
         self.add_customer("GR-LOYAL-100234", "Maria","6973658923")
         self.add_customer("GR-LOYAL-100569", "Theoni", "6982456781")
  
     # ------- clothingitems ---------
 
-    def add_clothingitem(self, barcode: int, description: str, brand: str, photos:list, available_colours: str, price:float ) -> ClassSession:
-        clothingitem =clothingitem(self._next_clothingitem_id, barcode, description, brand, photos, available_colours, price)
+    def add_clothingitem(self, barcode: int, description: str, brand: str, photos:list, available_colours: str, price:float ) -> ClothingItem:
+        clothingitem =ClothingItem(self._next_clothingitem_id, barcode, description, brand, photos, available_colours, price)
         self.clothingitems.append(clothingitem) # Add clothingitems to the list
         self._next_clothingitem_id += 1 # Increment clothingitem ID for next clothingitem
         return clothingitem
 
-    def list_clothingitems(self) -> list[clothingitem]:
+    def list_clothingitems(self) -> list[ClothingItem]:
         return self.clothingitems # Return the list of clothingitems
 
     def find_clothingitem_by_id(self, clothing_id: int) -> clothingitem | None:
@@ -40,6 +40,12 @@ class SupportSystem
         return None # No match found
 
     # ------- customers with loyalty card ---------
+
+    def add_customer(self, name: str, phone_number: int, loyalty_card: str) -> Customer:
+        customer = Customer (self._next_customer_id, name, phone_number, loyalty_card)
+        self.customer.append(customer) # Add customer to the list
+        self._next_clothingitem_id += 1 # Increment customer ID for next customer
+        return customer
 
     def find_or_create_customer(self, name: str, phone_number: int, loyalty_card:str) -> Customer:
         for m in self.customers:
@@ -52,16 +58,25 @@ class SupportSystem
 
     # ------- employees ---------
 
-    def employee_availability(self, employee_id: int) -> Employee | None:
-        employee = self.find_employee_by_id(employee_id)
-        if employee is None: # TODO: ελέγξτε αν  υπάρχει διαθέσιμος υπάλληλος
-            print("Δεν βρέθηκε διαθέσιμος υπάλληλος με αυτο το id.") 
-            return None 
+    def find_and_assign_employee(self) -> int:
+        for employee in self.employees:
+            # Έλεγχος αν ο υπάλληλος είναι διαθέσιμος
+            if employee.is_available():
+                # 1. Στέλνουμε το SMS στον υπάλληλο
+                print(f"Στάλθηκε SMS στον υπάλληλο με ID: {employee.emp_id()}")
+                
+                # 2. Αλλάζουμε τη διαθεσιμότητα σε False (δεν είναι πλέον διαθέσιμος ο υπάλληλος)
+                employee.availability = False
+                
+                # 3. Επιστρέφουμε το ID του πρώτου διαθέσιμου υπαλλήλου 
+                return employee.emp_id()
+            
+    # Αν η λούπα τελειώσει και δεν βρεθεί διαθέσιμος υπάλληλος
+        print("Δεν βρέθηκε διαθέσιμος υπάλληλος.")
+        return None 
+                
 
-        # TODO: ελέγξτε αν υπάρχει διαθέσιμος υπάλληλος
-        if not employee.is_available(): # Check for available employees
-            print("Δεν είναι διαθέσιμος.") # Print message if full
-            return None
+
 
         # TODO: αυξήστε συμμετέχοντες και δημιουργήστε Booking αντικείμενο
         session.add_participant() # Increment participants
